8e65962e4b1c66ab88acf6674e74e84451926849
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-01-12 12:32:42 -0600

    jobs added below my name

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -3,11 +3,20 @@ import "./style.css";
 const Intro = () => {
     return (
         <div className="intro">
-            <div className="intro-left">left</div>
+            <div className="intro-left">
                 <div className="intro-left-wrapper">
                     <h2 className="intro-tag">Hello, My name is</h2>
-                    <h2 className="intro-name">Jon Jackson</h2>
+                    <h1 className="intro-name">Jon Jackson</h1>
+                    <div className="intro-title">
+                        <div className="intro-title-wrapper">
+                            <div className='intro-title-item'>Web Developer</div>
+                            <div className='intro-title-item'>UI/UX Designer</div>
+                            <div className='intro-title-item'>Music Producer</div>
+                            <div className='intro-title-item'>Content Creator</div>
+                        </div>
+                    </div>
                 </div>
+            </div>
             <div className="intro-right">right</div>
         </div>
     )
