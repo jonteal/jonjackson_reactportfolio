565e07f46f8ddd8a3686c98125a85c528e657abe
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-01-15 16:44:59 -0600

    project list is working!

--- src/data.js
+++ src/data.js
@@ -2,27 +2,39 @@ import EatTheFrog from "./Images/eat-the-frog.png";
 import BrightIdeas from "./Images/brightideas.png";
 import DailyDev from "./Images/thedailydev.png";
 import WeatherAPI from "./Images/weather-dashboard.png";
+import NoteTaker from "./Images/note-taker.png";
+import ECommerceBackend from "./Images/e-commerce-backend.png";
 
 export const projects = [
     {
         id: 1,
-        img: {EatTheFrog},
+        img: EatTheFrog,
         link: "https://msp4msps.github.io/eat_the_frog",
     },
     {
         id: 2,
-        img: {BrightIdeas},
+        img: BrightIdeas,
         link: "https://gentle-wildwood-55104.herokuapp.com/",
     },
     {
         id: 3,
-        img: {DailyDev},
+        img: DailyDev,
         link: "https://thedailydev.herokuapp.com/",
     },
     {
         id: 4,
-        img: {WeatherAPI},
+        img: WeatherAPI,
         link: "https://jonteal.github.io/weather-dashboard/",
+    },
+    {
+        id: 5,
+        img: NoteTaker,
+        link: "https://github.com/jonteal/note-taker",
+    },
+    {
+        id: 6,
+        img: ECommerceBackend,
+        link: "https://github.com/jonteal/e-commerce-back-end",
     },
 ];
 
