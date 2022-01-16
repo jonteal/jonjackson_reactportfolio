adea60a656c64096158b9ba4996b6973e9f69faa
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-01-15 21:50:02 -0600

    light and dark mode are workingI icons look weird tho

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { useContext } from "react";
 import Intro from "./components/Intro";
 import About from "./components/About/about.js";
 import ProjectList from "./components/ProjectList/projectList.js";
 import Contact from "./components/Contact/contact.js";
 import Toggle from "./components/Toggle/toggle.js";
+import { ThemeContext } from "./context";
 
 const App = () => {
-const [dark,setDark] = useState(false)
+
+    const theme = useContext(ThemeContext);
+    const darkMode = theme.state.darkMode;
+
     return (
-        <div>
+        <div style={{ backgroundColor:darkMode ? "#222" : "white", 
+        color: darkMode && "white" }}>
             <Toggle/>
             <Intro/>
             <About/>
--- src/components/Toggle/toggle.js
+++ src/components/Toggle/toggle.js
@@ -1,13 +1,21 @@
 import "./toggle.css";
 import Sun from "../../Images/sun.png";
 import Moon from "../../Images/moon.png";
+import { useContext } from "react";
+import { ThemeContext } from "../../context";
 
 const Toggle = () => {
+    const theme = useContext(ThemeContext);
+
+    const handleClick = () => {
+        theme.dispatch({type:"TOGGLE"});
+    }
+    
     return (
         <div className="toggle">
             <img src={Sun} alt="" className="toggle-icon" />
             <img src={Moon} alt="" className="toggle-icon" />
-            <div className="toggle-button"></div>
+            <div className="toggle-button" onClick={handleClick} style={{left: theme.state.darkMode ? 0 : 25 }}></div>
         </div>
     )
 }
