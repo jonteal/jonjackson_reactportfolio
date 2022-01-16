7fae92faee07272625bc331e9178d8908c18a1d3
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-01-15 22:09:17 -0600

    css media queries for responsiveness mostly done. Needs more work though

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -2,12 +2,15 @@ import "./contact.css";
 import Phone from "../../Images/phone.png";
 import Email from "../../Images/email.png";
 import Address from "../../Images/address.png";
-import { useRef, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import emailjs from "emailjs-com";
+import { ThemeContext } from "../../context";
 
 const Contact = () => {
     const formRef = useRef();
     const [done, setDone] = useState(false);
+    const theme = useContext(ThemeContext);
+    const darkMode = theme.state.darkMode;
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -67,10 +70,10 @@ const Contact = () => {
                         your team or project. I look forward to hearing from you!
                     </p>
                     <form ref={formRef} onSubmit={handleSubmit}>
-                        <input type="text" placeholder="Name" name="user_name" />
-                        <input type="text" placeholder="Subject" name="user_subject" />
-                        <input type="text" placeholder="Email" name="user_email" />
-                        <textarea rows="5" placeholder="Message" name="message" />
+                        <input style={{backgroundColor: darkMode && "#333" }} type="text" placeholder="Name" name="user_name" />
+                        <input style={{backgroundColor: darkMode && "#333" }} type="text" placeholder="Subject" name="user_subject" />
+                        <input style={{backgroundColor: darkMode && "#333" }} type="text" placeholder="Email" name="user_email" />
+                        <textarea style={{backgroundColor: darkMode && "#333" }} rows="5" placeholder="Message" name="message" />
                         <button>Submit</button>
                         {done && "Thank you for your message!"}
                     </form>
