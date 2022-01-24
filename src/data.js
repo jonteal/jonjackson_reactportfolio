33dadfb618409e57d282e3a06c7c27baa44c98c3
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-01-24 00:10:13 -0600

    descriptions all fit in the mobile version of the project cards

--- src/data.js
+++ src/data.js
@@ -12,42 +12,42 @@ export const projects = [
         link: "https://github.com/msp4msps/eat_the_frog",
         live: "https://msp4msps.github.io/eat_the_frog/",
         title: "Eat the Frog",
-        description: "'Eat the Frog' is a task manager with a built-in incentive system for the user to help them stay more accountable and more productive."
+        description: "'Eat the Frog' is a front-end task manager designed to help users increase and maintain their productivity."
     },
     {
         id: 2,
         img: BrightIdeas,
         link: "https://github.com/EMtheDM/BrightIdeas",
         live: "https://gentle-wildwood-55104.herokuapp.com/",
-        description: "'Bright Ideas' is a simple and concise project management system where a user can create and manage their projects from start to finish"
+        description: "'Bright Ideas' is a full stack project management tool where users can manage projects from start to finish."
     },
     {
         id: 3,
         img: DailyDev,
         link: "https://github.com/jonteal/tech-blog",
         live: "https://thedailydev.herokuapp.com/",
-        description: "'The Daily Dev' is a tech blog for developers to have a place to share information about technical concepts and new technologies"
+        description: "'The Daily Dev' is a full stack tech blog as a place for developers to share information about technical concepts."
     },
     {
         id: 4,
         img: WeatherAPI,
         link: "https://github.com/jonteal/weather-dashboard",
         live: "https://jonteal.github.io/weather-dashboard/",
-        description: "'Weather API' is a dashboard for users look up the weather in any city and see the weather for that day as well as a 5-day forecast"
+        description: "'Weather Dashboard' is a 3rd party API tool designed to look up the weather in any city as well as a 5-day forecast."
     },
     {
         id: 5,
         img: NoteTaker,
         link: "https://github.com/jonteal/note-taker",
         live: "https://jonjackson-note-taker.herokuapp.com/",
-        description: "'NoteTaker' is a tool where a user could write and keep track of notes in order to remember important tasks or great lyrics"
+        description: "'NoteTaker' is a full stack tool for users to keep track of notes in order to remember important tasks."
     },
     {
         id: 6,
         img: GameofThrones,
         link: "https://github.com/jonteal/game-of-thrones-quiz",
         live: "https://jonteal.github.io/game-of-thrones-quiz/", 
-        description: "'Game of Thrones Quiz' is a timed quiz to test the player's trivial knowledge of the HBO series Game of Thrones. Winter is coming!"
+        description: "'Game of Thrones Quiz' is a quiz written in Javascript to test the player's trivial knowledge Game of Thrones."
     },
 ];
 
