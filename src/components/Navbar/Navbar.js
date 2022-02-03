47363b5d90e0eadeb1ed9a4cba31944fae881341
jonjackson <87287862+jonteal@users.noreply.github.com>
2022-02-03 15:58:21 -0600

    still didn't work. tried one more thing

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,5 @@
-// import React, { Component } from "react";
 import React, { useState } from 'react';
 import "./Navbar.css";
-// import Resume from "../../Images/jon-jackson-resume.pdf";
-
-// import { MenuItems } from "./MenuItems";
-// import GithubIcon from "../../Images/white-github-logo.png";
-// import LinkedInIcon from  "../../Images/white-linkedin-logo.png";
 
 
 function Navbar({ currentPage, handlePageChange }) {
@@ -13,17 +7,11 @@ function Navbar({ currentPage, handlePageChange }) {
     const [clicked, setClicked] = useState(false);
 
     const handleClick = () => {
-        // this.setState({ clicked: !this.state.clicked })
         setClicked(!clicked);
     }
 
-
-
     return (
         <nav className="navbar-items">
-                {/* <h1 className='name'>
-                    <span className='first-letter'>J</span>on <span className='first-letter'>J</span>ackson
-                </h1> */}
             <div className="menu-icon" onClick={handleClick}>
                 <i className={clicked ? "fas fa-times" : "fas fa-bars"}></i>
             </div>
@@ -77,52 +65,5 @@ function Navbar({ currentPage, handlePageChange }) {
         </nav>
     );
 }
-
-
-// class Navbar extends Component {
-
-//     state = { clicked: false }
-
-//     handleClick = () => {
-//         this.setState({ clicked: !this.state.clicked })
-//     }
-
-//     render() {
-//         return(
-//         <nav className="navbar-items">
-//             <div className="social-icons">
-//                 <a href="https://github.com/jonteal" target="_blank" rel="noreferrer">
-//                     <img 
-//                         src={GithubIcon}
-//                         alt=""
-//                         className="social-icon"
-//                     />
-//                 </a>
-//                 <a href="https://www.linkedin.com/in/jonathanjackson25/" target="_blank" rel="noreferrer">
-//                     <img 
-//                         src={LinkedInIcon}
-//                         alt=""
-//                         className="social-icon"
-//                     />
-//                 </a>
-//             </div>
-//             <div className="menu-icon" onClick={this.handleClick}>
-//                 <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
-//             </div>
-//             <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-//                 {MenuItems.map((item, index) => {
-//                     return (
-//                         <li key={index} className="nav-item">
-//                             <a className={item.cName} href={item.url}>
-//                                 {item.title}
-//                             </a>
-//                         </li>
-//                     )
-//                 })}
-//             </ul>
-//         </nav>
-//         )
-//     }
-// }
 
 export default Navbar
